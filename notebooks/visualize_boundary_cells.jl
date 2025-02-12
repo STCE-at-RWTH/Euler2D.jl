a878c1f39abf91d6439a24c8c475a1c84455e5d3
Jonas Minor <github@nc.seltsam.net>
2025-02-12 13:06:57 +0100

    Updated Pluto Notebook to visualize Boundary cells better

--- notebooks/visualize_boundary_cells.jl
+++ notebooks/visualize_boundary_cells.jl
@@ -20,7 +20,7 @@ begin
 end
 
 # ╔═╡ 1b1c0430-bacf-454a-a0bf-b00c7b723b48
-esim = load_cell_sim("../data/circular_obstacle_primal.celltape")
+esim = load_cell_sim("../data/hyperbola_obstacle_primal.celltape")
 
 # ╔═╡ 01380877-d1cb-4ac7-ad08-8c8c57d95dcc
 begin
@@ -28,26 +28,31 @@ begin
 	print(esim.cell_ids)
 end
 
-# ╔═╡ 35061132-50bf-4dd8-8178-ddf25a78b2f6
-function visualize_boundary_cells(esim) 
-	data_field = sign.(esim.cell_ids)
-	(xs, ys) = cell_centers(esim)
-	return heatmap(xs, ys, data_field', aspect_ratio=:equal, dpi=600, size=(1000,1000), xlabel=L"x", ylabel=L"y", title="Boundary Cells", titlefontface="Computer Modern")
-end
-
 # ╔═╡ a123f9d8-e6fe-4beb-974c-5c97ac24a8ea
-function circleShape(radius, center, start=-π, stop=0.0)
+function circleShape(radius=0.75, center=SVector(0.0, 0.0), start=-π, stop=0.0)
 	θ = LinRange(start, stop, 500)
 	center[1] .+ radius*sin.(θ), center[2] .+ radius*cos.(θ)
 end
 
-# ╔═╡ ab62cd5f-5724-4d82-86f3-832cb69936d6
-begin
-	RADIUS = 0.75
-	CENTER = SVector(0.0, 0.0)
-	p = visualize_boundary_cells(esim)
-	plot!(p, circleShape(RADIUS, CENTER))
+# ╔═╡ 3264d7c8-2fa3-4071-9a2d-6f40a7c12ff4
+function hyperbolaShape(a=1.0, b=1.0, h=0.0, k=0.0, ymax=1.5, ymin=-1.5)
+	t = LinRange(atan(ymin), atan(ymax), 500)
+	h .- a*sec.(t), k .- b*tan.(t)
 end
+
+# ╔═╡ 35061132-50bf-4dd8-8178-ddf25a78b2f6
+function visualize_boundary_cells(esim)
+	RADIUS = 0.75
+	colorpalette = palette([RGB(0.0, 0.0, 0.5), RGB(0.5,  0.5, 0.5), RGB(0.0, 0.75, 1.0)])
+	plotcol=RGB(1.0, 0.5, 0.0)
+	data_field = sign.(esim.cell_ids)
+	(xs, ys) = cell_centers(esim)
+	p = heatmap(xs, ys, data_field', color=colorpalette, aspect_ratio=:equal, dpi=600, size=(1000,1000), xlabel=L"x", ylabel=L"y", title="Boundary Cells", titlefontface="Computer Modern")
+	plot!(p, hyperbolaShape(), label="Obstacle", lc=plotcol, lw=4)
+end
+
+# ╔═╡ ab62cd5f-5724-4d82-86f3-832cb69936d6
+visualize_boundary_cells(esim)
 
 # ╔═╡ Cell order:
 # ╠═1b7ead20-2f14-11ef-28d1-03ad74bf304a
@@ -55,4 +60,5 @@ end
 # ╠═01380877-d1cb-4ac7-ad08-8c8c57d95dcc
 # ╠═35061132-50bf-4dd8-8178-ddf25a78b2f6
 # ╠═a123f9d8-e6fe-4beb-974c-5c97ac24a8ea
+# ╠═3264d7c8-2fa3-4071-9a2d-6f40a7c12ff4
 # ╠═ab62cd5f-5724-4d82-86f3-832cb69936d6
