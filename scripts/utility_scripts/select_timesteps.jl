da42be810986944b6688e139102c341532d1461d
Sasha Fleming <alexander.fleming@rwth-aachen.de>
2025-03-10 12:27:47 +0100

    add data filtering script

---
+++ scripts/utility_scripts/select_timesteps.jl
@@ -0,0 +1,23 @@
+# requires Julia 1.11
+
+using Euler2D
+using Accessors
+
+###
+# accepts file output file [time step indices]
+###
+function (@main)(ARGS)
+    @assert length(ARGS) >= 3
+    big_sim = load_cell_sim(ARGS[1])
+    tsteps = parse.(Int, ARGS[3:end])
+    @info "Creating new file with different time step data:" tsteps ARGS[2]
+    small_sim = CellBasedEulerSim(
+        big_sim.ncells,
+        length(tsteps),
+        big_sim.bounds,
+        big_sim.tsteps[tsteps],
+        big_sim.cell_ids,
+        big_sim.cells[tsteps],
+    )
+    Euler2D.write_cell_sim(ARGS[2], small_sim)
+end
