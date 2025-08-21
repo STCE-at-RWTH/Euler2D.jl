56006bbe5b60986fb6bc4cefb7a148ebf33e3255
Sasha Fleming <alexander.fleming@rwth-aachen.de>
2025-08-21 16:24:39 +0200

    Finish the riemann solver
    and add many tests!

--- src/riemann_solver/exact_riemann_solver.jl
+++ src/riemann_solver/exact_riemann_solver.jl
@@ -1,9 +1,4 @@
 # Method skeleton for the exact Riemann solver.
-# if NonlinearSolve.jl is available see <package root>/ext/ExactRiemannExt
-# for actual implementation details
-#
-# the stubs are here to remind you to install NonlinearSolve.jl, which is a hefty dependency for a feature you may not need
-
 # following the derviations in Smoller 1994
 # Shock Waves and Reaction-Diffisuion Equations
 
@@ -13,8 +8,8 @@ using LinearAlgebra
 using SimpleNonlinearSolve
 using StaticArrays
 
-using Euler2D: dimensionless_pressure, dimensionless_speed_of_sound
-using Euler2D: select_middle, project_state_to_normal
+using Euler2D: dimensionless_pressure, dimensionless_speed_of_sound, F_euler
+using Euler2D: select_middle, apply_coordinate_tform, change_of_basis_matrix
 
 """Pressure ratio ``P_r/P_l`` abbreviated pi"""
 function _π(uL, uR, gas)
@@ -105,15 +100,15 @@ end
 # Defined below lemma 18.5
 
 function T_1(ρ, P, v, x, gas)
-    return (f_1(x, gas) * ρ, exp(-x) * P, v + sqrt(gas.γ * P / ρ) * h_1(x, gas))
+    return (ρ = f_1(x, gas) * ρ, P = exp(-x) * P, v = v + sqrt(gas.γ * P / ρ) * h_1(x, gas))
 end
 
 function T_2(ρ, P, v, x, gas)
-    return (exp(x) * ρ, P, v)
+    return (ρ = exp(x) * ρ, P = P, v = v)
 end
 
 function T_3(ρ, P, v, x, gas)
-    return (f_3(x, gas) * ρ, exp(x) * P, v + sqrt(gas.γ * P / ρ) * h_3(x, gas))
+    return (ρ = f_3(x, gas) * ρ, P = exp(x) * P, v = v + sqrt(gas.γ * P / ρ) * h_3(x, gas))
 end
 
 """
@@ -177,30 +172,172 @@ function solve_for_jump_parameters(qL, qR, gas)
 end
 
 function to_ρPv(q, gas)
-    return (q[1], dimensionless_pressure(q, gas), q[2] / q[1])
+    return (ρ = q[1], P = dimensionless_pressure(q, gas), v = select_middle(q) ./ q[1])
+end
+
+sound_speed(state_prim, gas) = sqrt(gas.γ * state_prim.P / state_prim.ρ)
+
+function to_conserved(ρ, P, v, v_shear, gas)
+    v_all = pushfirst(v_shear, v)
+    ρv_all = ρ * v_all
+    ρE = P / (gas.γ - 1) + 0.5 * (ρv_all ⋅ v_all)
+    return SVector(ρ, ρv_all..., ρE)
+end
+
+"""
+Find shock speed for two states `uL`, `uR` connected by a shock.
+"""
+function shock_speed_via_RH(uL, uR, gas)
+    fL = F_euler(uL, gas)[:, 1]
+    fR = F_euler(uR, gas)[:, 1]
+    f_jump = fR - fL
+    u_jump = uR - uL
+    # we know that f_jump must be a scalar multiple of u_jump
+    # and in fact u_jump is an eigenvector of f'(u) in the small disturbance limit
+    return f_jump[1] / u_jump[1]
+end
+
+"""
+Find rarefaction structure for states connected to state `L` by a 1-rarefaction. 
+"""
+function rarefaction_1(ray, prim_L, cL, gas)
+    u = ((gas.γ - 1.0) * first(prim_L.v) + 2 * (cL + ray)) / (gas.γ + 1.0)
+    ρ = ((prim_L.ρ^gas.γ * (u - ray)^2) / (gas.γ * prim_L.P))^(1 / (gas.γ - 1))
+    P = prim_L.P * (ρ / prim_L.ρ)^gas.γ
+    return (ρ = ρ, P = P, v = u)
+end
+
+"""
+Find rarefaction structure for states connected to state `R` by a 3-rarefaction. 
+"""
+function rarefaction_3(ray, prim_R, cR, gas)
+    u = ((gas.γ - 1.0) * first(prim_R.v) - 2 * (cR - ray)) / (gas.γ + 1.0)
+    ρ = ((prim_R.ρ^gas.γ * (ray - u)^2) / (gas.γ * prim_R.P))^(1 / (gas.γ - 1))
+    P = prim_R.P * (ρ / prim_R.ρ)^gas.γ
+    return (ρ = ρ, P = P, v = u)
+end
+
+function riemann_states_and_speeds(qL, qR, gas)
+    x1, x2, x3 = solve_for_jump_parameters(qL, qR, gas)
+    one_shock = x1 < 0
+    three_shock = x3 < 0
+
+    # switch to ρ, P, v
+    qL_prim = to_ρPv(qL, gas)
+    qR_prim = to_ρPv(qR, gas)
+    # apply the transformation functions to find the states between qL and qR
+    # that are separated by a contact wave
+    qL_prim_★ = T_1(qL_prim.ρ, qL_prim.P, first(qL_prim.v), x1, gas)
+    qR_prim_★ = T_2(values(qL_prim_★)..., x2, gas)
+    @assert isapprox(qL_prim_★[3], qR_prim_★[3]; atol = 1.0e-8) # assert velocity constant across contact wave!
+    # contact discontinuitity propagates at this speed
+    contact_speed = qL_prim_★.v
+    @assert isapprox(qL_prim_★[2], qR_prim_★[2]; atol = 1.0e-8) # assert pressure constant across contact wave!
+    # qR_prim_appx = T_3(qR_prim_★..., x3, gas)
+
+    cL = dimensionless_speed_of_sound(qL, gas)
+    cR = dimensionless_speed_of_sound(qR, gas)
+    cL_★ = sound_speed(qL_prim_★, gas)
+    cR_★ = sound_speed(qR_prim_★, gas)
+
+    speeds_1 = if one_shock
+        # solve just the first component of RH
+        s = (qL_prim_★.ρ * qL_prim_★.v - qL[2]) / (qL_prim_★.ρ - qL[1])
+        (s, s)
+    else
+        (first(qL_prim.v) - cL, contact_speed - cL_★)
+    end
+
+    speeds_3 = if three_shock
+        s = (qR[2] - qR_prim_★.ρ * qR_prim_★.v) / (qR[2] - qR_prim_★.ρ)
+        (s, s)
+    else
+        (contact_speed + cR_★, first(qR_prim.v) + cR)
+    end
+    return (
+        one_shock,
+        speeds_1,
+        qL_prim,
+        qL_prim_★,
+        contact_speed,
+        three_shock,
+        speeds_3,
+        qR_prim_★,
+        qR_prim,
+    )
 end
 
 function solve_riemann_problem(ray, uL, uR, gas, new_coords)
-    qL = project_state_to_normal(uL, new_coords[:, 1])
-    qR = project_state_to_normal(uR, new_coords[:, 1])
-    if test_for_vacuum_in_solution(qL, qR, gas)
-        throw(DomainError((uL, uR), "Vacuum state forms between uL and uR!"))
+    to_normal_coords = change_of_basis_matrix(I, new_coords)
+    qL = apply_coordinate_tform(uL, to_normal_coords)
+    qR = apply_coordinate_tform(uR, to_normal_coords)
+    (
+        has_one_shock,
+        (sL_1, sL_2),
+        qL_prim,
+        qL_prim_★,
+        contact_speed,
+        has_three_shock,
+        (sR_1, sR_2),
+        qR_prim_★,
+        qR_prim,
+    ) = riemann_states_and_speeds(qL, qR, gas)
+
+    shear_v_L = popfirst(qL_prim.v)
+    shear_v_R = popfirst(qR_prim.v)
+    if ray < contact_speed
+        # 1-family
+        if has_one_shock
+            if ray < sL_1
+                return uL
+            end
+            state = to_conserved(values(qL_prim_★)..., shear_v_L, gas)
+            return apply_coordinate_tform(state, new_coords)
+        else
+            if ray < sL_1
+                return uL
+            elseif ray < sL_2
+                raref_state = rarefaction_1(ray, qL_prim, sound_speed(qL_prim, gas), gas)
+                state = to_conserved(values(raref_state)..., shear_v_L, gas)
+                return apply_coordinate_tform(state, new_coords)
+            else
+                state = to_conserved(values(qL_prim_★)..., shear_v_L, gas)
+                return apply_coordinate_tform(state, new_coords)
+            end
+        end
+    else
+        # 3-family
+        if has_three_shock
+            if ray > sR_2
+                return uR
+            end
+            state = to_conserved(values(qR_prim_★)..., shear_v_R, gas)
+            return apply_coordinate_tform(state, new_coords)
+        else
+            if ray < sR_1
+                state = to_conserved(values(qR_prim_★)..., shear_v_R, gas)
+                return apply_coordinate_tform(state, new_coords)
+            elseif ray < sR_2
+                raref_state = rarefaction_3(ray, qR_prim, sound_speed(qR_prim, gas), gas)
+                state = to_conserved(values(raref_state)..., shear_v_R, gas)
+                return apply_coordinate_tform(state, new_coords)
+            else
+                return uR
+            end
+        end
     end
-    x1, x2, x3 = solve_for_jump_parameters(qL, qR, gas)
-    # switch to ρ, P, v
-    qL_prim = to_ρPv(qL, gas)
-    q1_prim = T_1(qL_prim..., x1, gas)
-    q2_prim = T_2(q1_prim..., x2, gas)
-    q3_prim = T_3(q2_prim..., x3, gas)
-
-    #TODO pick which state
-    #TODO add the off-dimension components back in
-    #TODO convert back to "real" co-ordinates
 end
 
 end
 
+"""
+  ϕ_exact(uL, uR, n, gas)
+
+Compute the value of ``F(u)\\cdot n`` on a boundary with unit normal ``n`` using an exact Riemann solver. 
+
+"""
 function ϕ_exact(uL, uR, n, gas)
-    u_riemann = ExactRiemannSolverInternal.solve_riemann_problem(0, uL, uR, n, gas)
-    return F_euler(u_riemann, gas)
+    coords = orthonormal_basis(n)
+    u_riemann = ExactRiemannSolverInternal.solve_riemann_problem(0, uL, uR, coords, gas)
+    return F_euler(u_riemann, gas) * n
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,55 +1,103 @@
 using Euler2D
+using LinearAlgebra
 using ShockwaveProperties
 using StaticArrays
 using Test
 using Unitful
 
-@testset "Nondimensionalisation" begin
-    using Euler2D: EulerEqnsScaling, _SI_DEFAULT_SCALE
-    using Euler2D: nondimensionalize, redimensionalize
-    using Euler2D: dimensionless_speed_of_sound, dimensionless_total_enthalpy_density
+@testset "Euler2D" verbose = true begin
+    @testset "Utilities" begin
+        using Euler2D:
+            orthonormal_basis, change_of_basis_matrix, change_basis, apply_coordinate_tform
+        v = @SVector [2.0, 1.0, 1.0, 2.0]
 
-    u1 = ConservedProps(PrimitiveProps(0.1u"kg/m^3", SVector(2.0, 0.0), 225.0u"K"), DRY_AIR)
-    a = speed_of_sound(u1, DRY_AIR)
-    scale = EulerEqnsScaling(1.0u"m", 1.0u"kg/m^3", a)
+        n1 = SVector(sqrt(2.0) / 2, sqrt(2.0) / 2)
+        B = orthonormal_basis(n1)
+        to_B = change_of_basis_matrix(I, B)
+        @test isapprox(B[:, 1] ⋅ B[:, 2], 0.0; atol = 1.0e-12)
 
-    u1_star = nondimensionalize(u1, scale)
-    a1_star = dimensionless_speed_of_sound(u1_star, DRY_AIR)
-    @test u1_star[2] / u1_star[1] ≈ 2.0
-    @test a1_star ≈ 1.0
-end
+        v_prime = apply_coordinate_tform(v, to_B)
+        @test v_prime isa SVector
+        @test v_prime[3] == 0
+        @test all(
+            tpl -> isapprox(tpl...; atol = 1.0e-12),
+            zip(v, apply_coordinate_tform(v_prime, B)),
+        )
+    end
 
-@testset "Exact Riemann Solver" begin
-    using Euler2D: ExactRiemannSolverInternal
+    @testset "Nondimensionalisation" begin
+        using Euler2D: EulerEqnsScaling, _SI_DEFAULT_SCALE
+        using Euler2D: nondimensionalize, redimensionalize
+        using Euler2D: dimensionless_speed_of_sound, dimensionless_total_enthalpy_density
 
-    # test for states only in x-axis
-    ux_1 = @SVector [1.0, 1.0, 0.0, 6000.0]
-    ux_2 = @SVector [1.0, 0.5, 0.0, 6000.0]
-    ux_3 = @SVector [2.0, 1.0, 0.0, 6000.0]
-    ux_4 = @SVector [1.0, -1.0, 0.0, 6000.0]
-    ux_5 = @SVector [2.0, -1.0, 0.0, 6000.0]
+        u1 = ConservedProps(
+            PrimitiveProps(0.1u"kg/m^3", SVector(2.0, 0.0), 225.0u"K"),
+            DRY_AIR,
+        )
+        a = speed_of_sound(u1, DRY_AIR)
+        scale = EulerEqnsScaling(1.0u"m", 1.0u"kg/m^3", a)
 
-    @test all(
-        ExactRiemannSolverInternal.jump_ratios(ux_1, ux_1, DRY_AIR) .== (1.0, 1.0, 0.0),
-    )
-    @test all(
-        x -> isapprox(x, 0.0; atol = 1.0e-12),
-        ExactRiemannSolverInternal.solve_for_jump_parameters(ux_1, ux_1, DRY_AIR),
-    )
-    @testset for (uL, uR) ∈
-                 [(ux_1, ux_2), (ux_2, ux_1), (ux_1, ux_4), (ux_4, ux_5), (ux_4, ux_2)]
-        (x1, x2, x3) = ExactRiemannSolverInternal.solve_for_jump_parameters(uL, uR, DRY_AIR)
-        primL = ExactRiemannSolverInternal.to_ρPv(uL, DRY_AIR)
-        primL_★ = ExactRiemannSolverInternal.T_1(primL..., x1, DRY_AIR)
-        primR_★ = ExactRiemannSolverInternal.T_2(primL_★..., x2, DRY_AIR)
-        primR_appx = ExactRiemannSolverInternal.T_3(primR_★..., x3, DRY_AIR)
-        primR = ExactRiemannSolverInternal.to_ρPv(uR, DRY_AIR)
-        @test primR_appx[1] ≈ primR[1]
-        @test primR_appx[2] ≈ primR[2]
-        @test primR_appx[3] ≈ primR[3]
+        u1_star = nondimensionalize(u1, scale)
+        a1_star = dimensionless_speed_of_sound(u1_star, DRY_AIR)
+        @test u1_star[2] / u1_star[1] ≈ 2.0
+        @test a1_star ≈ 1.0
+    end
 
-        three_wave = ExactRiemannSolverInternal.test_3_wave_for_rarefaction(uL, uR, DRY_AIR)
-        @test isapprox(x3, 0.0; atol = 1.0e-12) ||
-              ((three_wave && x3 < 0) && (!three_wave && x3 > 0))
+    @testset "Exact Riemann Solver" begin
+        using Euler2D: ExactRiemannSolverInternal
+
+        # test for states only in x-axis
+        ux_1 = @SVector [1.0, 1.0, 0.0, 6000.0]
+        ux_2 = @SVector [1.0, 0.0, 0.0, 6000.0]
+        ux_3 = @SVector [2.0, 1.0, 0.0, 6000.0]
+        ux_4 = @SVector [1.0, -1.0, 0.0, 6000.0]
+        ux_5 = @SVector [2.0, -1.0, 0.0, 6000.0]
+
+        @test all(
+            ExactRiemannSolverInternal.jump_ratios(ux_1, ux_1, DRY_AIR) .== (1.0, 1.0, 0.0),
+        )
+        @test all(
+            x -> isapprox(x, 0.0; atol = 1.0e-12),
+            ExactRiemannSolverInternal.solve_for_jump_parameters(ux_1, ux_1, DRY_AIR),
+        )
+        @testset for (uL, uR) ∈
+                     [(ux_1, ux_2), (ux_2, ux_1), (ux_1, ux_4), (ux_4, ux_5), (ux_4, ux_2)]
+            (x1, x2, x3) =
+                ExactRiemannSolverInternal.solve_for_jump_parameters(uL, uR, DRY_AIR)
+            primL = ExactRiemannSolverInternal.to_ρPv(uL, DRY_AIR)
+            @test primL[3] isa SVector
+            primL_★ =
+                ExactRiemannSolverInternal.T_1(primL[1], primL[2], primL[3][1], x1, DRY_AIR)
+            primR_★ = ExactRiemannSolverInternal.T_2(primL_★..., x2, DRY_AIR)
+            primR_appx = ExactRiemannSolverInternal.T_3(primR_★..., x3, DRY_AIR)
+            primR = ExactRiemannSolverInternal.to_ρPv(uR, DRY_AIR)
+            @test isapprox(primR_appx.ρ, primR.ρ; atol = 1.0e-12)
+            @test isapprox(primR_appx.P, primR.P; atol = 1.0e-12)
+            @test isapprox(primR_appx.v, first(primR.v); atol = 1.0e-12)
+            one_wave =
+                ExactRiemannSolverInternal.test_1_wave_for_rarefaction(uL, uR, DRY_AIR)
+            three_wave =
+                ExactRiemannSolverInternal.test_3_wave_for_rarefaction(uL, uR, DRY_AIR)
+            @test (
+                isapprox(x1, 0.0; atol = 1.0e-12) ||
+                ((one_wave && x1 > 0) || (!one_wave && x1 < 0))
+            )
+            @test (
+                isapprox(x3, 0.0; atol = 1.0e-12) ||
+                ((three_wave && x3 > 0) || (!three_wave && x3 < 0))
+            )
+
+            data_pack =
+                ExactRiemannSolverInternal.riemann_states_and_speeds(uL, uR, DRY_AIR)
+            @test data_pack[1] == !one_wave
+            @test data_pack[6] == !three_wave
+            # speeds should be increasing left-to-right!
+            @show one_wave, three_wave
+            @test data_pack[2][1] ≤
+                  data_pack[2][2] ≤
+                  data_pack[5] ≤
+                  data_pack[7][1] ≤
+                  data_pack[7][2]
+        end
     end
 end
